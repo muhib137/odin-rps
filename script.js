058d4a614139abfc3f429c8dc920b3e35b31ebf1
Muhibbulloh <225215017+muhib137@users.noreply.github.com>
2025-08-12 13:27:25 +0500

    Get human choice and refactor

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-
 const ROCK = "rock";
 const PAPER = "paper";
 const SCISSORS = "scissors";
 
-
-
-function getComputerChoice(){
-  const randInt = Math.floor(Math.random() * 3);
-  let computerChoice;
-  switch(randInt){
+function getChoice(index) {
+  switch (index) {
     case 0:
-      computerChoice = ROCK;
-      break;
-    case 1: 
-      computerChoice = PAPER;
-      break;
+      return ROCK;
+    case 1:
+      return PAPER;
     default:
-      computerChoice = SCISSORS;
+      return SCISSORS;
   }
-  
+}
+
+function getComputerChoice() {
+  const randInt = Math.floor(Math.random() * 3);
+  const computerChoice = getChoice(randInt);
   return computerChoice;
+}
+
+function getHumanChoice() {
+  const humanChoice = parseInt(
+    prompt("Enter the corresponding number:\n0 - rock\n1 - paper\n2 - scissors")
+  );
+  return getChoice(humanChoice);
 }
