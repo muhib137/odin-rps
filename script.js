c06b9208ad83473a648f51d125d78f32b25e7181
Muhibbulloh <225215017+muhib137@users.noreply.github.com>
2025-08-12 16:31:05 +0500

    Play one round

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const ROCK = "rock";
 const PAPER = "paper";
 const SCISSORS = "scissors";
+const PLAYER_HUMAN = "human";
+const PLAYER_COMPUTER = "computer";
+
+let computerScore = 0,
+  humanScore = 0;
 
 function getChoice(index) {
   switch (index) {
@@ -25,3 +30,37 @@ function getHumanChoice() {
   );
   return getChoice(humanChoice);
 }
+
+function findWinner(humanChoice, computerChoice) {
+  let winner;
+  if (humanChoice === ROCK && computerChoice === SCISSORS) {
+    winner = PLAYER_HUMAN;
+    humanScore++;
+  } else if (humanChoice === PAPER && computerChoice === ROCK) {
+    winner = PLAYER_HUMAN;
+    humanScore++;
+  } else if (humanChoice === SCISSORS && computerChoice === PAPER) {
+    winner = PLAYER_HUMAN;
+    humanScore++;
+  } else if (humanChoice === computerChoice) {
+    winner = null;
+  } else {
+    winner = PLAYER_COMPUTER;
+    computerScore++;
+  }
+
+  return winner;
+}
+
+function playRound(humanChoice, computerChoice) {
+  const winner = findWinner(humanChoice, computerChoice);
+  const msg = winner ? `Congrats. ${winner} is the winner!` : "It's a draw.";
+  console.log(`(You) ${humanChoice} vs ${computerChoice} (Computer)`);
+  console.log(msg);
+  console.log(`Score -  (You) ${humanScore}:${computerScore} (computer)`);
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
